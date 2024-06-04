7272ac8b5d6fd2fcbd2c4338509bc8c0439a3784
Sawyerinvi <Eugene_tut@mail.ru>
2024-06-04 08:46:24 +0500

    Animation fix

--- Assets/Scripts/FPCMovement.cs
+++ Assets/Scripts/FPCMovement.cs
@@ -2,42 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FPCMovement : MonoBehaviour
+public class FPCMovement : StateMachineBehaviour
 {
     private Animator animator;
-    private CameraPosition cameraPosition = CameraPosition.FacingCenter;
-    void Start()
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator = GetComponent<Animator>();
-    }
-
-    void FixedUpdate()
-    {
-        if (Input.mousePosition.x < Screen.width / 6 && cameraPosition == CameraPosition.FacingCenter)
+        if (Input.mousePosition.x < Screen.width / 6)
         {
-            cameraPosition = CameraPosition.FacingLeft;
             animator.SetTrigger("Left");
         }
-        if (Input.mousePosition.x < Screen.width / 6 && cameraPosition == CameraPosition.FacingRight)
+        if (Input.mousePosition.x > Screen.width / 6 * 5)
         {
-            cameraPosition = CameraPosition.FacingCenter;
-            animator.SetTrigger("Left");
-        }
-        if (Input.mousePosition.x > Screen.width / 6 * 5 && cameraPosition == CameraPosition.FacingLeft)
-        {
-            cameraPosition = CameraPosition.FacingCenter;
-            animator.SetTrigger("Right");
-        }
-        if (Input.mousePosition.x > Screen.width / 6 * 5 && cameraPosition == CameraPosition.FacingCenter)
-        {
-            cameraPosition = CameraPosition.FacingRight;
             animator.SetTrigger("Right");
         }
     }
-}
-public enum CameraPosition
-{
-    FacingRight,
-    FacingLeft,
-    FacingCenter
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Right");
+        animator.ResetTrigger("Left");
+    }
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Right");
+        animator.ResetTrigger("Left");
+    }
 }
