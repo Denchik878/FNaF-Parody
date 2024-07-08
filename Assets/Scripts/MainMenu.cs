f8c41fe69476352508ee0c6f31d656b67b9462d6
Denchik878 <148837802+Denchik878@users.noreply.github.com>
2024-07-08 10:52:21 -0700

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainPanel;
+    public GameObject optionPanel;
     public void Play()
     {
-
+        SceneManager.LoadScene(1);
+    }
+    public void Option()
+    {
+        mainPanel.SetActive(false);
+        optionPanel.SetActive(true);
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
